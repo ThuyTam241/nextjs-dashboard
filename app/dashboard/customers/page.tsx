08a4b0695955616c7d285d17a2fafa681dfaada1
ThuyTam241 <thuytam241510@gmail.com>
2025-07-09 09:17:35 +0700

    feature: implement invoice and customer tables

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,24 @@
-import { fetchCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-export default async function Page() {
-  const customers = await fetchCustomers();
+export default async function Page(props: {
+  searchParams?: Promise<{ query: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query ?? "";
 
-  return <CustomersTable customers={customers} />;
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<CustomersTableSkeleton />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
